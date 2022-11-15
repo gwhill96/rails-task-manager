0b9988f54520e3e85d85fe200cfb0c13bbdba7a8
George Hill <gwhill96@gmail.com>
2022-11-15 16:10:36 +0000

    Basic functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  
+
+  # # Read
+  # get "tasks", to: "tasks#index"
+
+  # # Create
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
+
+  # # Read one
+  # get "tasks/:id", to: "tasks#show", as: :task
+
+  # #Update
+  # get "task/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#edit"
+
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
 end
